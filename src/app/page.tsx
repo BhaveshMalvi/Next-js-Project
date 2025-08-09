0a8aed1dee02ae423a49d67a7fe70761d37e61f0
BhaveshMalvi <bhaveshkumarmalvi87@gmail.com>
2025-08-09 17:42:49 +0530

    change name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import WhyChooseUs from "@/components/WhyChooseUs";
 export default function Home() {
   return (
     <main className=" min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className=" text-center text-3xl">Hello This is Home Page...</h1>
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
